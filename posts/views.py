0613b5f234b7753659452160e7ab122e529490ac
Asya Haroyan <alharoyan@gmail.com>
2024-10-26 06:14:30 +0000

    Add filter in posts

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from api_inspira.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -18,8 +19,17 @@ class PostList(generics.ListCreateAPIView):
     ).order_by('-created_at')
     filter_backends = [
         filters.OrderingFilter,
-        filters.SearchFilter
+        filters.SearchFilter,
+        DjangoFilterBackend,
         ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+        'style',
+        'location',
+        'area_type',
+    ]
     search_fields = [
         'owner__username',
         'title',
