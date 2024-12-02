533351862a871ab7c9fca27f1022196a5ad305dd
Asya Haroyan <alharoyan@gmail.com>
2024-12-02 10:51:33 +0000

    Fix bug, change the name of the area type view

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('posts/', views.PostList.as_view()),
     path('posts/<int:pk>/', views.PostDetail.as_view()),
     path('posts/style/', views.PostStyleChoice.as_view(), name='style_choices'),
-    path('posts/area_type/', views.PostStyleChoice.as_view(), name='area_type_choices'),
+    path('posts/area_type/', views.PostAreaChoice.as_view(), name='area_type_choices'),
 ]
