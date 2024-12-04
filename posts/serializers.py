d071426332c9e308e6eba2949ad6451b4336ef5b
Asya Haroyan <alharoyan@gmail.com>
2024-12-04 08:59:22 +0000

    Change the profile image source

--- posts/serializers.py
+++ posts/serializers.py
@@ -7,7 +7,7 @@ class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.avatar.url')
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
